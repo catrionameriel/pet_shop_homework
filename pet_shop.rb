7bc15175869097bff9c77967f41ce33f0e102c2d
catrionameriel <catriona.meriel@gmail.com>
2017-11-03 15:12:06 +0000

    test_add_or_remove_cash passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -2,6 +2,12 @@ def pet_shop_name(shop)
   shop[:name]
 end
 
+
 def total_cash(shop)
   shop[:admin][:total_cash]
+end
+
+
+def add_or_remove_cash(shop, amount)
+  shop[:admin][:total_cash] = total_cash(shop) + amount
 end
